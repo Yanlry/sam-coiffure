d48dfe7e1ea6f0f65e4521e935602e69b415d164
Yanlry <contact.54@icloud.com>
2024-10-14 11:05:17 +0200

    Etablissement du script de deffilement

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
+// Afficher l'image dans le carrousel de la page index
 document.addEventListener("DOMContentLoaded", function() {
-    // CAROUSEL
-    const galleryGrid = document.querySelector('.gallery-grid-home');
+    const galleryGrid = document.querySelector('.home-gallery-grid');
     if (galleryGrid) {
-        const originalItems = Array.from(galleryGrid.children); // Récupère les éléments d'origine
-        const cloneCount = 2; // Nombre d'éléments à cloner
+        const originalItems = Array.from(galleryGrid.children); 
+        const cloneCount = 2; 
 
         // Cloner les premiers éléments et les ajouter à la fin
         for (let i = 0; i < cloneCount; i++) {
-            const clone = originalItems[i].cloneNode(true); // Clone l'élément
-            galleryGrid.appendChild(clone); // Ajoute le clone à la fin de la galerie
+            const clone = originalItems[i].cloneNode(true); 
+            galleryGrid.appendChild(clone); 
         }
 
         let scrollAmount = 0;
-        const scrollSpeed = 2; // Pixels à défiler à chaque intervalle
-        const intervalSpeed = 20; // Millisecondes entre chaque appel
+        const scrollSpeed = 2;
+        const intervalSpeed = 20;
 
         function autoScroll() {
             scrollAmount += scrollSpeed;
@@ -30,79 +30,97 @@ document.addEventListener("DOMContentLoaded", function() {
             setInterval(autoScroll, intervalSpeed);
         }
     }
-
-    // SLIDER D'AVIS
-    let slideIndex = 0;
-    const slides = document.getElementsByClassName("avis-slide");
-
-    function showSlides(n) {
-        if (slides.length) {
-            if (n >= slides.length) { slideIndex = 0; }
-            if (n < 0) { slideIndex = slides.length - 1; }
-            for (let i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-            slides[slideIndex].style.display = "block";
-        }
-    }
-
-    // Afficher la première slide au chargement
-    showSlides(slideIndex);
-
-    // NAVIGATION POUR LES SLIDES
-    const plusSlides = (n) => {
-        slideIndex += n;
-        showSlides(slideIndex);
-    }
-
-    // Menu overlay/hamburger et contact
-    const menuIcon = document.getElementById('menu-icon');
-    const navLinks = document.getElementById('nav-links');
-    const overlay = document.getElementById('overlay');
-    const closeIcon = document.getElementById('close-icon');
-    const contactIcon = document.getElementById('contact-icon');
-    const contactMenu = document.getElementById('contact-menu');
-    const closeContactIcon = document.getElementById('close-contact-icon');
-
-    if (menuIcon && navLinks && overlay) {
-        // Ouvrir le menu hamburger
-        menuIcon.addEventListener('click', () => {
-            navLinks.classList.add('active');
-            overlay.classList.add('active');
-        });
-
-        // Fermer le menu hamburger via l'overlay
-        overlay.addEventListener('click', () => {
-            navLinks.classList.remove('active');
-            contactMenu.classList.remove('active');
-            overlay.classList.remove('active');
-        });
-
-        // Fermer le menu hamburger via la croix
-        closeIcon.addEventListener('click', () => {
-            navLinks.classList.remove('active');
-            overlay.classList.remove('active');
-        });
-
-        // Ouvrir le menu de contact
-        contactIcon.addEventListener('click', () => {
-            contactMenu.classList.add('active');
-            overlay.classList.add('active');
-        });
-
-        // Fermer le menu de contact via la croix
-        closeContactIcon.addEventListener('click', () => {
-            contactMenu.classList.remove('active');
-            overlay.classList.remove('active');
-        });
-    }
 });
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Défilement des avis
+let slideIndex = 0;
+const slides = document.getElementsByClassName("avis-slide");
+
+function showSlides(n) {
+    if (slides.length) {
+        // S'assurer que l'indice est dans les limites
+        if (n >= slides.length) { slideIndex = 0; }
+        if (n < 0) { slideIndex = slides.length - 1; }
+
+        // Masquer toutes les slides
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+
+        // Afficher la slide courante
+        slides[slideIndex].style.display = "block";
+    }
+}
+
+// Afficher la première slide au chargement
+showSlides(slideIndex);
+
+// Navigation pour les slides
+function plusSlides(n) {
+    // Incrémente ou décrémente l'index de la slide
+    slideIndex += n;
+
+    // Afficher la nouvelle slide après changement d'indice
+    showSlides(slideIndex);
+}
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Menu overlay/hamburger et contact
+const menuIcon = document.getElementById('menu-icon');
+const navLinks = document.getElementById('nav-links');
+const overlay = document.getElementById('overlay');
+const closeIcon = document.getElementById('close-icon');
+const contactIcon = document.getElementById('contact-icon');
+const contactMenu = document.getElementById('contact-menu');
+const closeContactIcon = document.getElementById('close-contact-icon');
+
+if (menuIcon && navLinks && overlay) {
+    // Ouvrir le menu hamburger
+    menuIcon.addEventListener('click', () => {
+        navLinks.classList.add('active');
+        overlay.classList.add('active');
+    });
+
+    // Fermer le menu via l'overlay
+    overlay.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+        contactMenu.classList.remove('active');
+        overlay.classList.remove('active');
+    });
+
+    // Fermer le menu via la croix
+    closeIcon.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+        overlay.classList.remove('active');
+    });
+
+    // Ouvrir le menu de contact
+    contactIcon.addEventListener('click', () => {
+        contactMenu.classList.add('active');
+        overlay.classList.add('active');
+    });
+
+    // Fermer le menu de contact via la croix
+    closeContactIcon.addEventListener('click', () => {
+        contactMenu.classList.remove('active');
+        overlay.classList.remove('active');
+    });
+}
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Afficher l'image dans le carrousel de la page galerie
 let currentIndex = 0;
 const images = document.querySelectorAll('.carousel-image');
 const modal = document.getElementById('galleryModal');
 const modalThumbnails = document.querySelectorAll('.modal-thumbnail');
 
-// Afficher l'image dans le carrousel
 function showImage(index) {
     images.forEach((img, i) => {
         img.classList.remove('active');
@@ -135,25 +153,40 @@ function closeModal() {
     modal.style.display = 'none';
 }
 
-// Lorsque l'on clique sur une miniature dans le modal
+// Afficher l'image sélectionnée dans le carrousel et fermer le modal
 function showModalImage(index) {
-    showImage(index); // Affiche l'image sélectionnée dans le carrousel
-    closeModal(); // Ferme le modal
+    showImage(index);
+    closeModal();
 }
 
-// Événement de fermeture du modal si on clique en dehors
+// Fermer le modal si on clique en dehors
 window.onclick = function(event) {
     if (event.target == modal) {
         closeModal();
     }
 }
 
-document.querySelectorAll('.service-item-home').forEach(item => {
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Gestion de la section services sur la page d'accueil et sur la page services
+document.querySelectorAll('.home-service-item, .service-item').forEach(item => {
     item.addEventListener('click', () => {
         // Supprimer la classe active de tous les services
-        document.querySelectorAll('.service-item-home').forEach(el => el.classList.remove('active'));
+        document.querySelectorAll('.home-service-item, .service-item').forEach(el => el.classList.remove('active'));
         
         // Ajouter la classe active à l'élément cliqué
         item.classList.add('active');
+    });
+});
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+
+document.querySelectorAll('.price-item').forEach(item => {
+    item.addEventListener('click', function() {
+        // Supprimer la classe active de tous les items
+        document.querySelectorAll('.price-item').forEach(el => el.classList.remove('active'));
+
+        // Ajouter la classe active à l'élément cliqué
+        this.classList.add('active');
     });
 });
